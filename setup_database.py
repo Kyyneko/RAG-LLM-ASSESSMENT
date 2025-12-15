ce0e555699dc7f672d7a98042f7b3ea6d79c5fa3
Kyyneko <mahendrakirana284@gmail.com>
2025-12-15 08:49:55 +0800

    feat: Add GitHub Actions workflow for VPS deployment and comment out database index setup.

--- setup_database.py
+++ setup_database.py
@@ -1,6 +1,6 @@
-from db.connection import setup_database_indexes
+# from db.connection import setup_database_indexes
 
-if __name__ == "__main__":
-    print("Menyiapkan database indexes...")
-    setup_database_indexes()
-    print("Selesai!")
+# if __name__ == "__main__":
+#     print("Menyiapkan database indexes...")
+#     setup_database_indexes()
+#     print("Selesai!")
